8703a7238d5e008d96c6423b45b1dcdfdbd1f2cd
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-12-11 15:10:04 +0700

    Update Database

--- src/db/db_afiliya.sql
+++ src/db/db_afiliya.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 05, 2023 at 12:38 PM
--- Server version: 10.6.15-MariaDB-cll-lve
--- PHP Version: 8.1.16
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2023 at 09:07 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,23 +44,30 @@ CREATE TABLE `tb_comments` (
 --
 
 CREATE TABLE `tb_orders` (
-  `order_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `shop_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `refferal` varchar(8) NOT NULL,
+  `order_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `shop_id` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `refferal` varchar(8) DEFAULT NULL,
+  `delivery_service_id` int(10) NOT NULL,
+  `payment_method_id` int(10) NOT NULL,
+  `address_id` int(10) NOT NULL,
   `amount` int(100) NOT NULL,
-  `createAt` date NOT NULL DEFAULT current_timestamp(),
-  `updateAt` date NOT NULL DEFAULT current_timestamp()
+  `price` int(255) NOT NULL,
+  `cost` int(255) NOT NULL,
+  `total_payment` int(255) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'Menunggu Pembayaran',
+  `createdAt` date NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pay_transactions`
+-- Table structure for table `tb_payments`
 --
 
-CREATE TABLE `tb_pay_transactions` (
+CREATE TABLE `tb_payments` (
   `payment_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `delivery_service_id` int(11) NOT NULL,
@@ -82,11 +89,11 @@ CREATE TABLE `tb_pay_transactions` (
 --
 
 CREATE TABLE `tb_products` (
-  `product_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
   `shop_id` int(11) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `product_price` int(50) NOT NULL,
-  `product_image` varchar(100) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
   `product_stock` int(255) NOT NULL,
   `product_ctg` varchar(100) NOT NULL,
   `weight` int(11) NOT NULL,
@@ -94,8 +101,8 @@ CREATE TABLE `tb_products` (
   `height` int(11) NOT NULL,
   `width` int(11) NOT NULL,
   `description` text NOT NULL,
-  `createAt` date NOT NULL DEFAULT current_timestamp(),
-  `updateAt` date NOT NULL DEFAULT current_timestamp()
+  `createdAt` date NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
@@ -105,37 +112,30 @@ CREATE TABLE `tb_products` (
 --
 
 CREATE TABLE `tb_shops` (
-  `shop_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `shop_id` varchar(255) NOT NULL,
+  `user_id` varchar(225) NOT NULL,
   `shop_name` varchar(50) NOT NULL,
   `shop_ctg` varchar(50) NOT NULL,
   `telephone` varchar(20) NOT NULL,
   `address` varchar(100) NOT NULL,
   `description` text NOT NULL,
-  `createAt` date NOT NULL DEFAULT current_timestamp(),
-  `updateAt` date NOT NULL DEFAULT current_timestamp()
+  `createdAt` date NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  `shop_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_shop_transactions`
+-- Table structure for table `tb_shops_history`
 --
 
-CREATE TABLE `tb_shop_transactions` (
-  `shp_transc_id` int(11) NOT NULL,
-  `payment_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `delivery_service_id` int(11) NOT NULL,
-  `payment_method_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `address_id` int(11) NOT NULL,
-  `shop_id` int(11) NOT NULL,
-  `refferal` varchar(8) NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `price` int(255) NOT NULL,
-  `createAt` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `tb_shops_history` (
+  `history_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `order_id` varchar(255) NOT NULL,
+  `createdAt` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -144,21 +144,35 @@ CREATE TABLE `tb_shop_transactions` (
 --
 
 CREATE TABLE `tb_users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `isVerification` int(1) NOT NULL,
+  `isVerification` int(1) NOT NULL DEFAULT 0,
   `telephone` varchar(20) NOT NULL,
-  `gender` date NOT NULL,
-  `birthplace` date NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `birthplace` varchar(50) NOT NULL,
   `birthdate` date NOT NULL,
-  `image` varchar(50) NOT NULL,
-  `isAffiliator` int(1) NOT NULL,
-  `isShop` int(1) NOT NULL,
-  `createAt` date NOT NULL DEFAULT current_timestamp(),
-  `updateAt` date NOT NULL DEFAULT current_timestamp()
+  `image` varchar(255) DEFAULT NULL,
+  `isAffiliator` int(1) NOT NULL DEFAULT 0,
+  `isShop` int(1) NOT NULL DEFAULT 0,
+  `token` varchar(255) DEFAULT NULL,
+  `createdAt` date NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_users_history`
+--
+
+CREATE TABLE `tb_users_history` (
+  `history_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `order_id` varchar(255) NOT NULL,
+  `createdAt` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -175,25 +189,6 @@ CREATE TABLE `tb_user_address` (
   `address_details` text NOT NULL,
   `placename` varchar(50) NOT NULL,
   `recipient_info` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_user_transactions`
---
-
-CREATE TABLE `tb_user_transactions` (
-  `user_transc_id` int(11) NOT NULL,
-  `payment_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `delivery_service_id` int(11) NOT NULL,
-  `payment_method_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `address_id` int(11) NOT NULL,
-  `shop_id` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `createdAt` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
@@ -227,9 +222,9 @@ ALTER TABLE `tb_orders`
   ADD PRIMARY KEY (`order_id`);
 
 --
--- Indexes for table `tb_pay_transactions`
+-- Indexes for table `tb_payments`
 --
-ALTER TABLE `tb_pay_transactions`
+ALTER TABLE `tb_payments`
   ADD PRIMARY KEY (`payment_id`);
 
 --
@@ -245,12 +240,6 @@ ALTER TABLE `tb_shops`
   ADD PRIMARY KEY (`shop_id`);
 
 --
--- Indexes for table `tb_shop_transactions`
---
-ALTER TABLE `tb_shop_transactions`
-  ADD PRIMARY KEY (`shp_transc_id`);
-
---
 -- Indexes for table `tb_users`
 --
 ALTER TABLE `tb_users`
@@ -261,12 +250,6 @@ ALTER TABLE `tb_users`
 --
 ALTER TABLE `tb_user_address`
   ADD PRIMARY KEY (`address_id`);
-
---
--- Indexes for table `tb_user_transactions`
---
-ALTER TABLE `tb_user_transactions`
-  ADD PRIMARY KEY (`user_transc_id`);
 
 --
 -- Indexes for table `tb_user_verification`
@@ -285,40 +268,10 @@ ALTER TABLE `tb_comments`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tb_orders`
+-- AUTO_INCREMENT for table `tb_payments`
 --
-ALTER TABLE `tb_orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_pay_transactions`
---
-ALTER TABLE `tb_pay_transactions`
+ALTER TABLE `tb_payments`
   MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_products`
---
-ALTER TABLE `tb_products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_shops`
---
-ALTER TABLE `tb_shops`
-  MODIFY `shop_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_shop_transactions`
---
-ALTER TABLE `tb_shop_transactions`
-  MODIFY `shp_transc_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_users`
---
-ALTER TABLE `tb_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_user_address`
